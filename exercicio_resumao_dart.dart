88643c0c35de2769c7f368a3a5f1b5949726756d
Bowie <fecastilhos@gmail.com>
2025-05-12 17:18:28 -0300

    exercicio_resumao_dart

---
+++ exercicio_resumao_dart.dart
@@ -0,0 +1,60 @@
+main () {
+ /*
+  dynamic x = 'Teste";
+  x = 123;
+  x = false;
+
+  print(x);
+
+  var a = 3;
+  a = 4;
+
+  final b = 3;
+  // b = 6;
+
+  const c = 5 ;
+  c = 7;
+
+  print ('Primeiro programa');
+  int a = 3;
+  double b = 3.1;
+  bool estaChovendo = true;
+  bool estaFrio = false;
+  var c = 'Vc é muito legal';
+  print (c is String);
+
+  print (estaChovendo || estaFrio);
+  var nomes = [ 'Ana', 'Bia', 'Carlos'];
+  nomes.add('Daniel');
+  nomes.add('Daniel');
+  nomes.add('Daniel');
+  print(nomes.length);
+  print(nomes.elementAt(0));
+  print(nomes[5]);
+
+  var conjunto = {0,1,2,3,4,4,4};
+  Set<int>conjunto = {0,1,2,3,4,4,4};
+  print(conjunto.length);
+  print(conjunto is Set);
+
+  Map<String, double>notaDosAlunos = {
+    'Ana': 9.7,
+    'Bia': 9.2,
+    'Carlos':7.8,
+  };
+  
+  
+  for(var chave in notaDosAlunos.keys{
+    print('chave=$chave');
+  }
+  for(var valor in notaDosAlunos.values){
+    print ('valor = $valor');
+  }
+  for (var registro in notaDosAlunos.entries){
+    print ('${registro.key} = ${registro.value}');
+  }
+*/
+
+
+
+}
