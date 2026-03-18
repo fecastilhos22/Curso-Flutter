0d2e888c8166741815ec203e96f587e90916d47b
Bowie <fecastilhos@gmail.com>
2026-03-18 16:23:57 -0300

    tipos basicos

--- fundamentos_dart/constantes_1.dart
+++ fundamentos_dart/constantes_1.dart
@@ -7,13 +7,14 @@ main() {
 
 const PI = 3.1415; // const  = quando o valor já está pré-definido
 
-//String texto = stdin.readLineSync();
+//String entradaDoUsuario = stdin.readLineSync();
 //print ("O valor digitado é :" + texto);
+
 
 //para ficar tudo na mesma linha stdout.write("Informe o raio: "); ao invés do print
 print("Informe o raio: ");
 final entradaDoUsuario = stdin.readLineSync();
-final double raio = double.parse(entradaDoUsuario);
+//final double raio = double.parse(entradaDoUsuario);
 
 final area = PI * raio * raio;
 print("O Valor do raio é : " + area.toString());
